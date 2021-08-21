07e8da9f74cc7c55539ae906030d6ce78a712ac2
Qazi Fahim Farhan <zephyrus@Qazis-iMac.local>
2021-08-21 22:39:28 +0600

    :construction: Create a cardView

---
+++ DilyScrumStartingPoint/Scrumdinger/CardView.swift
@@ -0,0 +1,42 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Qazi Fahim Farhan on 21/8/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
+
+    }
+//    .padding()
+//    .foregroundColor(scrum.color.acc)
+}
+
+
+struct CardView_Previews: PreviewProvider{
+    static var scrum = DailyScrum.data[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
