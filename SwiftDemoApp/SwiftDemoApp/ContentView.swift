0397ed2bd8f7726fdcdc41d1fd3af7a7f392e147
Qazi Fahim Farhan <zephyrus@Qazis-iMac.local>
2021-08-21 20:32:41 +0600

    :tada: Create first project

---
+++ SwiftDemoApp/SwiftDemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemoApp
+//
+//  Created by Qazi Fahim Farhan on 21/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftDemoApp/SwiftDemoApp/SwiftDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoAppApp.swift
+//  SwiftDemoApp
+//
+//  Created by Qazi Fahim Farhan on 21/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
