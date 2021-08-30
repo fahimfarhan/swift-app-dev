0305ae60d8937d3cf667e209526a0db1eff0474d
Qazi Fahim Farhan <zephyrus@Qazis-iMac.local>
2021-08-31 01:06:21 +0600

    :zap: Copy paste few lines from navigation tutorial

---
+++ CreatingNavigationHierarchy/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,31 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }.listRowBackground(scrum.color)
+            }
+        }.navigationTitle("Daily Scrum")
+        .navigationBarItems(trailing: Button(action: {}) {
+            Image(systemName: "plus")
+        })
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        
+        }
+    }
+}
